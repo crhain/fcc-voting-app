f7addb58fa85f84b59dc2e92b1e482f260272ca3
Carl Hain <crhain@gmail.com>
2017-06-01 22:16:50 -0500

    Clean up app.js

--- app.js
+++ app.js
@@ -78,8 +78,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//basic route
-app.use('/', index);
+
+//app.use(require('./middlewares/users'));
+//app.use('/', index);
+//set up routes
+app.use(require('./routes/index.js'));
 
 //establish connection to database
 db.connect(process.env.DATABASE, (err, db) =>{
--- models/users.js
+++ models/users.js
@@ -1,1 +1,4 @@
 const db = require('./database.js');
+const bcrypt = require('bcrypt');
+
+//add hash, authenticate, and other user related functions and database calls
