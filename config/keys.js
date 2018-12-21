fa68efaaaf7a65de1625190a5bbf8b3fc91e8be9
Carl Hain <crhain@gmail.com>
2018-12-21 11:31:18 -0600

    Add config file with dev.js, keys.js, and prod.js files to hold configuration for application

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID:
+      '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+    googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+    mongoURI: 'mongodb://crhain:makEG_oos7@ds263493.mlab.com:63493/blog_dev',
+    cookieKey: '123123123'
+};
