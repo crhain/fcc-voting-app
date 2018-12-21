91f6d66a201643b7e4ebc22cf134b899ade45913
Carl Hain <crhain@gmail.com>
2018-12-21 11:27:07 -0600

    Revert "Create config directory with keys.js, dev.js, and prod.js files that store configuration data for both dev server and production server; currently these are not connected to the app"
    This reverts commit 9d3b332dd864e9c9af572cd0f87007366e6a1b07.

--- config/prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    DATABASE: process.env.DATABASE,
-    SESSION_SECRET: process.env.SESSION_SECRET,
-    TWITTER_CLIENT_ID: process.env.TWITTER_CLIENT_ID,
-    TWITTER_CLIENT_SECRET: process.env.TWITTER_CLIENT_SECRET
-};
