9d3b332dd864e9c9af572cd0f87007366e6a1b07
Carl Hain <crhain@gmail.com>
2018-12-21 10:47:59 -0600

    Create config directory with keys.js, dev.js, and prod.js files that store configuration data for both dev server and production server; currently these are not connected to the app

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    DATABASE: 'mongodb://crhain:rabiddogs_07frogging@ds161001.mlab.com:61001/crh-vote-app',
+    SESSION_SECRET: '43affm556asdfk',
+    TWITTER_CLIENT_ID: 'zVHDaJLzW8Ofy1CApIynaXvcS',
+    TWITTER_CLIENT_SECRET: '5dXkGtjbPtFNLCJ4e5Tsh3evdCBV8woBGKkLVXSbp1OsScmve2'
+};
+
+
